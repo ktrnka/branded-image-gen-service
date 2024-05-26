b7169df078e987af9e0417c5b265eee693974af1
Keith Trnka <keith.trnka@gmail.com>
2024-05-26 11:53:47 -0700

    Connect to OpenAI API

--- backend/main.py
+++ backend/main.py
@@ -2,15 +2,22 @@ from fastapi import FastAPI
 from txtai.embeddings import Embeddings
 from .data import companies, prompt_templates
 import random
+from openai import OpenAI
 
+# Index the brands
 embeddings = Embeddings(content=True, path="BAAI/bge-small-en-v1.5")
 embeddings.index([f"{company['market']}\n{company['brand_identity']}" for company in companies])
 
+# Setup OpenAI
+openai_client = OpenAI()
+
+# Setup the API
 api = FastAPI()
 
 def adjust_prompt(prompt: str, company_name: str) -> str:
     template = random.choice(prompt_templates)
     return template.format(prompt=prompt, company_name=company_name)
+
 
 @api.get("/")
 def route():
@@ -33,3 +40,27 @@ def augment(prompt: str):
         })
 
     return response
+
+@api.get("/generate")
+def generate(prompt: str):
+    result = embeddings.search(prompt, limit=1)[0]
+    company_index = int(result['id'])
+    company = companies[company_index]
+    augmented_prompt = adjust_prompt(prompt, company["name"])
+
+    response = openai_client.images.generate(
+        model="dall-e-3",
+        prompt=augmented_prompt,
+        size="1024x1024",
+        quality="standard",
+        n=1,
+    )
+
+    response_data = response.data[0]
+
+    return {
+        "company": company["name"],
+        "company_match_score": result["score"],
+        "prompt": augmented_prompt,
+        "openai_response": response_data
+    }
