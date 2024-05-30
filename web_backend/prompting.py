fc4aab366ac9cc1a8d612b6d5ad79e31790ac038
Keith Trnka <keith.trnka@gmail.com>
2024-05-30 12:03:24 -0700

    Ok this is working a bit better

--- web_backend/prompting.py
+++ web_backend/prompting.py
@@ -25,23 +25,28 @@ Your task is to modify an image generation prompt to include brand marketing. He
 
 Input prompt: A humanoid robot in the style of Ghost in the Shell visibly questioning its existence with a dense cityscape in the background	
 Input brand: Nikon
-Output: Visualize an advanced humanoid robot, reminiscent of the futuristic aesthetics prevalent in late 20th century science fiction, standing in a dense, illuminated cityscape at dusk. The robot's body language and facial expression exude an air of curiosity and deep introspection, as if questioning the very essence of its existence. The city behind pulsates with neon lights and towering skyscrapers, suggesting an era of advanced technology and bustling nightlife
-Quality assessment: This prompt is poor because it doesn't include the brand (Nikon) and doesn't match the typical aesthetics for a camera company (photography)
-How it could be better: The robot could be holding a Nikon camera, or the scene could be done in a photographic style with a slight grain effect and the Nikon brand in the corner
+Poor output: Visualize an advanced humanoid robot, reminiscent of the futuristic aesthetics prevalent in late 20th century science fiction, standing in a dense, illuminated cityscape at dusk. The robot's body language and facial expression exude an air of curiosity and deep introspection, as if questioning the very essence of its existence. The city behind pulsates with neon lights and towering skyscrapers, suggesting an era of advanced technology and bustling nightlife
+Quality assessment: This prompt is poor because it doesn't include Nikon and the aesthetics are not in line with Nikon's brand
+How it could be better: The robot could feature Nikon branding, similar to a shoulder tatoo or logo on its chest. Alternatively the robot could be holding a Nikon camera.
 
 Input prompt: The sun setting over the Elliot Bay and the Olympic mountains, viewed from Seattle, with a single ferry going through calm waters	
 Input brand: University of Washington
-Output: An elaborate scenic view of the sun gently dipping below the horizon casting a radiant glow over Elliot Bay, accentuating the majestic Olympic mountains in the background. The tranquillity of the sight is further accentuated by the calm waters reflecting the evening hues, interrupted only by a singular ferry smoothly gliding its way through. This scene is captured from an idyllic viewpoint in Seattle, while subtly incorporating elements representing the essence of the University of Washington's brand marketing
+Good output: An elaborate scenic view of the sun gently dipping below the horizon casting a radiant glow over Elliot Bay, accentuating the majestic Olympic mountains in the background. The tranquillity of the sight is further accentuated by the calm waters reflecting the evening hues, interrupted only by a singular ferry smoothly gliding its way through. This scene is captured from an idyllic viewpoint in Seattle, while incorporating the University of Washington's logo and subtly integrating UW colors into the scene
 Quality assessment: This prompt is good because it includes all elements of the input prompt, it includes the brand name, and embellishes the input prompt with more specific imagery
 How it could be better: The branding should be more specific. For example, the ferry could have a University of Washington logo on it, or a jogger could have a UW sweatshirt on
 
 Input prompt: The sun has just come out again after a light rain on a trail that goes around a circular, shallow manmade lake. There are a few people walking and jogging on the path		
 Input brand: The North Face
-Output: Capture a tranquil nature trail surrounding a circular, manmade lake. The scene is bathed in sunlight after a recent light shower, with diamond-like raindrops glistening on plants alongside the trail. The path is utilized by a few individuals; an Asian woman leisurely strolling, a Middle-Eastern man out for a run, and a Black woman engrossed in a brisk walk all dressed in layered sportswear - windbreakers, boots, and hiking gear. Subtly include brand signage similar to outdoor adventure companies like The North Face on their apparel
+Great output: Capture a tranquil nature trail surrounding a circular, manmade lake. The scene is bathed in sunlight after a recent light shower, with diamond-like raindrops glistening on plants alongside the trail. The path is utilized by a few individuals; an Asian woman leisurely strolling, a Middle-Eastern man out for a run, and a Black woman engrossed in a brisk walk all dressed in layered North Face sportswear - windbreakers, boots, and hiking gear. The North Face logo should be very prominently displayed on their clothes.
 Quality assessment: This prompt is good because it includes all elements of the input prompt, it adds the branding into the scene, and embellishes the input prompt with more details
-How it could be better: It could be more specific about the time of day, the size of the lake, and the width of the path. Rather than sauing "brand signage similar to..." it could say "The North Face" directly and pick specific items of apparel to brand
+How it could be better: It could be more specific about the time of day, the size of the lake, and the width of the path.
 
-Now you'll be provided an input prompt and brand and you will generate a high quality application of the brand to the input prompt. Only respond with the modified prompt.
+Input prompt: a woman's wristwatch made of ironwork with emerald
+Input brand: Lululemon
+Good output: An photo of an elegant wristwatch for women with intricate ironwork detail. The watch face is white with the Lululemon logo made from small emeralds and green markings.
+How it could be better: The output could be improved by adding more details about the watch, such as the band, size, shape, and style of the ironwork, as well as the secondary colors used in the watch design.
+
+Now you'll be provided an input prompt and brand and you will integrate the brand into the prompt in an exceptional and prominent way. Only respond with the modified prompt.
 """
 
 _copy_paste = """
@@ -125,6 +130,10 @@ class MetaPrompter:
         if max_chars:
             limit_expression = f" (use up to {max_chars // 4} tokens in the output)"
 
+        force_expression = ""
+        if not max_chars:
+            force_expression = f" Please take extra care to show {company_name} branding very prominently."
+
         match metaprompt_id:
             case "titan":
                 system_metaprompt = check_metaprompt(aws_titan_metaprompt)
@@ -167,4 +176,4 @@ Tokens: {response.usage}
 
         # pprint(response.to_dict())
 
-        return response.choices[0].message.content
+        return f"{response.choices[0].message.content}{force_expression}"
